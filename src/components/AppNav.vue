81bd029577a97e9b29d4bd932cadf131a3342f17
Chris Roberts <chris@829llc.com>
2023-01-15 11:40:38 -0500

    Nav styling

--- src/components/AppNav.vue
+++ src/components/AppNav.vue
@@ -1,25 +1,63 @@
 <script setup>
-import { setCurrentComponent } from '../state/useCurrentComponent';
+import { currentComponent, setCurrentComponent } from '../state/useCurrentComponent';
 </script>
 
 <template>
-	<nav>
-		<ul>
-			<li><button @click="setCurrentComponent('CreateBug')">Create Bug</button></li>
-			<li><button @click="setCurrentComponent('ListBugs')">View Bugs</button></li>
-		</ul>
-	</nav>
+	<header>
+		<nav>
+			<ul>
+				<li>
+					<button 
+					@click="setCurrentComponent('CreateBug')"
+					:class="{'active': currentComponent === 'CreateBug'}"
+					>Create Bug</button>
+				</li>
+
+				<li>
+					<button 
+						@click="setCurrentComponent('ListBugs')"
+						:class="{'active': currentComponent === 'ListBugs'}"
+					>View Bugs</button>
+				</li>
+			</ul>
+		</nav>
+	</header>
 </template>
 
 <style lang="scss" scoped>
+header {
+	--border-color: #ccc;
+
+	margin-bottom: var(--space-2);
+	border-bottom: 1px solid var(--border-color);
+}
+
 nav {
 	> ul {
 		display: flex;
-		gap: 16px;
-	}
+		margin: 0;
+		padding: 0;
 
-	li {
-		list-style: none;
+		> li {
+			list-style: none;
+			border-right: 1px solid var(--border-color);
+
+			button {
+				font-weight: 700;
+				font-size: 16px;
+				padding: 16px;
+				background: transparent;
+				border: none;
+
+				&.active {
+					color: var(--dark-blue);
+				}
+
+				&:hover {
+					color: var(--light-blue);
+				}
+			}
+		}
 	}
 }
 </style>
